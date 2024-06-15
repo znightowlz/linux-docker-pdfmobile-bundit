83f5731bfb9046293e9f90bbf5d55d2c2bb98e92
znightowlz <126089711+znightowlz@users.noreply.github.com>
2024-06-16 00:13:46 +0700

    firstCommit Clone From Window

---
+++ cornetto.py
@@ -0,0 +1,7 @@
+class corndog:
+    #เชื่อมต่อฐานข้อมูล PostgresQL
+    DB_NAME = "pdfmobile_bundit"
+    DB_USER = "postgres"
+    DB_PASSWORD = "pf2615"
+    DB_HOST = "localhost"
+    DB_PORT = "5432"
