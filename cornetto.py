956801d67bf5ff0754371521e610900a0c376eb3
znightowlz <126089711+znightowlz@users.noreply.github.com>
2024-06-18 09:11:32 +0700

    delete All

--- cornetto.py
+++
@@ -1,7 +0,0 @@
-class corndog:
-    #เชื่อมต่อฐานข้อมูล PostgresQL
-    DB_NAME = "pdfmobile_bundit"
-    DB_USER = "postgres"
-    DB_PASSWORD = "pf2615"
-    DB_HOST = "localhost"
-    DB_PORT = "5432"
